cd54ef3075d0ba84b16c93186d1e271418b6d846
James Russo <jdr289@cornell.edu>
2016-10-02 23:32:00 -0400

    implemented baseline predictor

--- nlp_project2_uncertainty/POStagger.py
+++ nlp_project2_uncertainty/POStagger.py
@@ -7,7 +7,9 @@ class POStagger():
     self.train_lines = []
     self.hmmtrainer = nltk.tag.HiddenMarkovModelTrainer()
     self.hmmtagger = None
+    self.baseline_dictionary = {}
 
+  # parse the training directory putting BIO CUE's for all CUES
   def parse_training_files(self, directory):
     for file_name in glob.glob(directory + "/*.txt"):
       with open(file_name, 'r') as article:
@@ -42,6 +44,7 @@ class POStagger():
             self.train_lines.append(curr_sent)
             curr_sent = []
 
+  # parse the testing directory
   def parse_testing_files(self, directory):
     tagged_test = []
     for file_name in glob.glob(directory + "/*.txt"):
@@ -58,9 +61,31 @@ class POStagger():
             curr_sent = []
     return tagged_test
 
+  # build a baseline uncertainty dictionary where all uncertain tokens are in it
+  def build_baseline_dict(self):
+    for sentence in self.train_lines:
+      for tup in sentence:
+        if ("CUE" in tup[1]) and (tup[0] not in self.baseline_dictionary):
+          self.baseline_dictionary[tup[0]] = tup[1]
+
+  def predict_baseline(self, test_list):
+    predicted = []
+    for line in test_list:
+      curr_sent = []
+      for token in line:
+        if token in self.baseline_dictionary:
+          curr_sent.append((token, "B-CUE"))
+        else:
+          curr_sent.append((token, "O"))
+      predicted.append(curr_sent)
+
+    return predicted
+
+  # wrapper function for the HMM training method provided by nltk
   def hmm_train(self):
     self.hmmtagger = self.hmmtrainer.train_supervised(self.train_lines)
 
+  # wrapper function for the HMM tagging method provided by nltk
   def hmm_predict(self, test_list):
     predicted = []
     for line in test_list:
@@ -76,7 +101,11 @@ def main():
   tagger.parse_training_files("train")
   tagger.hmm_train()
 
+  tagger.build_baseline_dict()
+
   public = tagger.parse_testing_files("test-public")
+
+  baseline_public_predicted = tagger.predict_baseline(public)
 
   predicted = tagger.hmm_predict(public)
 
